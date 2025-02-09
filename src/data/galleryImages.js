e19b72b4048e9600c1d0cee0fffca40204f93d18
ᴹᴹᵂ <93227950+h4ckm4t@users.noreply.github.com>
2025-02-09 16:20:22 +0100

    Update galleryImages.js

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -1,87 +1,88 @@
+// /src/data/galleryImages.js
 
 export const galleryImages = [
     {
       id: 1,
       url: '/images/1.jpeg',  // ścieżka względem folderu public
-      title: 'Widok z zewnątrz',
-      description: 'Frontowa część domu'
+      title: 'Widok na basen',
+//      description: 'Frontowa część domu'
     },
     {
       id: 2,
       url: '/images/2.jpeg',
-      title: 'Basen', 
-      description: 'Prywatny basen z leżakami'
+      title: 'Basen',
+//      description: 'Prywatny basen z leżakami'
     },
     {
       id: 3,
       url: '/images/3.jpeg',
-      title: 'Salon',
-      description: 'Przestronny salon z dostępem do tarasu'
+      title: 'Taras',
+//      description: 'Przestronny salon z dostępem do tarasu'
     },
     {
       id: 4,
       url: '/images/4.jpeg',
       title: 'Taras',
-      description: 'Taras z widokiem na basen i ogród'
+//      description: 'Taras z widokiem na basen i ogród'
     },
     {
       id: 5,
       url: '/images/5.jpeg',
-      title: 'Kuchnia',
-      description: 'W pełni wyposażona kuchnia z jadalnią'
+      title: 'Ogród',
+//      description: 'Znajdziesz prysznic w palmach?'
     },
     {
       id: 6,
       url: '/images/6.jpeg',
-      title: 'Sypialnia',
-      description: 'Główna sypialnia z łazienką'
+      title: 'Prysznic',
+//      description: 'Główna sypialnia z łazienką'
     },
     {
       id: 7,
       url: '/images/7.jpeg',
-      title: 'Łazienka',
-      description: 'Główna łazienka z prysznicem'
+      title: 'Salon',
+//      description: 'Główna łazienka z prysznicem'
     },
     {
       id: 8,
       url: '/images/8.jpeg',
-      title: 'Sypialnia gościnna',
-      description: 'Sypialnia z dwoma łóżkami'
+      title: 'Sypialnia',
+//      description: 'Sypialnia z dwoma łóżkami'
     },
     {
       id: 9,
       url: '/images/9.jpeg',
-      title: 'Taras na dachu',
-      description: 'Taras z widokiem na okolicę'
+      title: 'Łazienka 1',
+//      description: 'Taras z widokiem na okolicę'
     },
     {
       id: 10,
       url: '/images/10.jpeg',
-      title: 'Ogród',
-      description: 'Widok na ogród'
+      title: 'Łazienka 2',
+//      description: 'Widok na ogród'
     },
     {
       id: 11,
       url: '/images/11.jpeg',
-      title: 'Okolica',
-      description: 'Widok na okolicę'
+      title: 'Bokój B',
+      description: 'Pojedyńcze łóżko'
     },
     {
       id: 12,
       url: '/images/12.jpeg',
-      title: 'Plaża',
-      description: 'Pobliska plaża'
+      title: 'Pokój A',
+      description: 'Piętrowe łóżko'
     },
     {
       id: 13,
       url: '/images/13.jpeg',
-      title: 'Wieczorny widok',
-      description: 'Dom o zachodzie słońca'
+      title: 'Zachód Słońca',
+//      description: ''
     },
     {
       id: 14,
       url: '/images/14.jpeg',
-      title: 'Basen nocą',
-      description: 'Oświetlony basen wieczorem'
+      title: 'Morze Śródziemne',
+//      description: 'Oświetlony basen wieczorem'
     }
   ];
